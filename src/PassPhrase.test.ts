e777a4adc973b93ec93d4b37cae3fc04cebb1749
shmansa <124243497+shmansa@users.noreply.github.com>
2023-03-09 01:47:16 -0800

    Added unit test case to check 'is_caps_locked'

--- src/PassPhrase.test.ts
+++ src/PassPhrase.test.ts
@@ -17,4 +17,17 @@ describe("PassPhrase tests", ()=> {
     let actual: number = myPhrase.length();
     expect(actual).toBe(12);
   });
+  
+  test("is_caps_locked returns true for a phrase with opposite case alphabetic characters", () => {
+    // 1. set up the state
+    let myPhrase: PassPhrase = new PassPhrase("PaSsPhRaSe");
+    let attempt: string = "pAsSpHrAsE";
+  
+    // 2. invoke action
+    let actual: boolean = myPhrase.is_caps_locked(attempt);
+
+    // 3. assert expected outcome
+    expect(actual).toBe(true);
+  });
+  
 })
