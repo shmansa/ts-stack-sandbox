a15eab14765e1188fe64eef70a3d82b6f49cfe28
shmansa <124243497+shmansa@users.noreply.github.com>
2023-03-09 10:00:38 -0800

    Added test case to check 'has_mixed_case'

--- src/PassPhrase.test.ts
+++ src/PassPhrase.test.ts
@@ -30,4 +30,21 @@ describe("PassPhrase tests", ()=> {
     expect(actual).toBe(true);
   });
   
+  test("has_mixed_case returns false for a phrase with only upper case characters", () => {
+    let myPhrase: PassPhrase = new PassPhrase("THISISATEST");
+    let actual: boolean = myPhrase.has_mixed_case();
+    expect(actual).toBe(false);
+  });
+
+  test("has_mixed_case returns true for a phrase with both upper and lower case characters", () => {
+    let myPhrase: PassPhrase = new PassPhrase("ThisIsATest");
+    let actual: boolean = myPhrase.has_mixed_case();
+    expect(actual).toBe(true);
+  });
+
+  test("has_mixed_case returns false for a phrase with only lower case characters", () => {
+    let myPhrase: PassPhrase = new PassPhrase("thisisatest");
+    let actual: boolean = myPhrase.has_mixed_case();
+    expect(actual).toBe(false);
+  });
 })
